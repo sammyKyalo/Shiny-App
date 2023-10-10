30dbc791e413d2cbf9ef257b78bdad1f93200543
Sammy Kyalo <sammykyalo47@gmail.com>
2023-10-10 21:59:55 +0300

    changes

---
+++ login_page.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import sqlite3
+from passlib.hash import pbkdf2_sha256
+import subprocess
+
+
+# Create a connection to the SQLite database
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
+
+# Create a table to store user information (if it doesn't exist)
+c.execute('''
+          CREATE TABLE IF NOT EXISTS users 
+          (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          username TEXT, 
+          email TEXT, 
+          password TEXT)
+          ''')
+conn.commit()
+
+st.set_page_config(page_title='TESLA Stock Analysis Login Page', page_icon=':chart_with_upwards_trend:')
+custom_css = """
+<style>
+.zoom-text {
+    font-family: 'Arial', sans-serif;
+    font-size: 50px;
+    text-align: center;
+    position: relative;
+    color: #ff4500; /* Tesla brand color */
+    text-transform: uppercase;
+    animation: zoom 5s infinite alternate ease-in-out;
+    }
+
+    @keyframes zoom {
+        0% {
+            transform: scale(1);
+        }
+        100% {
+        transform: scale(1.1);
+        }
+    }
+</style>
+"""
+
+# Display the zooming in and out title
+st.markdown(custom_css, unsafe_allow_html=True)
+st.markdown('<div class="zoom-text">TESLA Stock Analysis and Prediction</div>', unsafe_allow_html=True)
+
+
+# Functions for database interactions
+def create_user(username, email, password):
+    hashed_password = pbkdf2_sha256.hash(password)
+    c.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (username, email, hashed_password))
+    conn.commit()
+
+def verify_user(username, password):
+    c.execute("SELECT * FROM users WHERE username=?", (username,))
+    user = c.fetchone()
+    if user and pbkdf2_sha256.verify(password, user[3]):
+        return True
+    return False
+
+# Selectbox for Login and Sign Up options
+selection = st.selectbox("Select Option(sign up/login)", ["Login", "Sign Up"])
+
+# Login Section
+if selection == "Login":
+    st.header("Login")
+    username = st.text_input('Username')
+    password = st.text_input('Password', type='password')
+    if st.button('Login'):
+        if verify_user(username, password):
+            st.success(f'Welcome, {username}!')
+            
+            # Run shiny.py and display the output here
+            subprocess.run(["streamlit", "run", "shiny.py"])
+            st.session_state.username_key = ""  
+            st.session_state.password_key = ""  
+
+            login_button = False
+        else:
+            st.error('Invalid credentials. Please try again.')
+            st.info('If you don\'t have an account, please sign up.')
+
+# Signup Section
+else:
+    st.header("Sign Up")
+    new_username = st.text_input('Create Username')
+    new_email = st.text_input('Email')
+    new_password = st.text_input('Create Password', type='password')
+    if st.button('Sign Up'):
+        create_user(new_username, new_email, new_password)
+        st.success(f'Account created for {new_username}! You can now login.')
+
+# Close the database connection
+conn.close()
--- shiny.py
+++ shiny.py
@@ -10,18 +10,70 @@ from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pmdarima as pm
+import sqlite3
+import time
 
-# Set page title and favicon
+
+
 st.set_page_config(page_title='TESLA Stock Analysis', page_icon=':chart_with_upwards_trend:')
 
-# Title of the app
-st.title('TESLA Stock Analysis and Prediction')
+custom_css = """
+<style>
+.zoom-text {
+    font-family: 'Arial', sans-serif;
+    font-size: 50px;
+    text-align: center;
+    position: relative;
+    base="light";
+    backgroundColor="#E6C7C7";
+    layout="wide";
+    color: #ff4500; /* Tesla brand color */
+    text-transform: uppercase;
+    animation: zoom 5s infinite alternate ease-in-out;
+    }
 
-# Sidebar for user input
+    @keyframes zoom {
+        0% {
+            transform: scale(1);
+        }
+        100% {
+        transform: scale(1.1);
+        }
+    }
+</style>
+"""
+
+
+# Display the zooming in and out title
+st.markdown(custom_css, unsafe_allow_html=True)
+st.markdown('<div class="zoom-text">TESLA Stock Analysis and Prediction</div>', unsafe_allow_html=True)
+
+
+col1, col2 = st.sidebar.columns([1, 1])  # Split the sidebar into 2 columns
+with col1:
+    st.write("Click to Update Output")
+    button = st.button('Start Processing')  # Button to trigger the progress bar update
+with col2:
+    spinner = st.spinner() 
+
 st.sidebar.header('User Input')
+
+
+
+
+
+
+
+
+
+
+
+
 ticker = st.sidebar.text_input('Enter Ticker Symbol', 'TSLA')
 start_date_input = st.sidebar.date_input('Start Date', datetime.today() - timedelta(days=4*365))
 end_date_input = st.sidebar.date_input('End Date', datetime.today())
+
+
 
 # Fetch stock data
 try:
@@ -31,34 +83,98 @@ except Exception as e:
     st.sidebar.error(f'Error loading data: {e}')
     st.stop()
 
-# Candlestick chart
+if button:
+    with spinner:  
+        data = yf.download(ticker, start_date_input, end_date_input)
+        if data is not None:
+            success_message = st.empty()  
+            success_message.success(f'Data for {ticker} has been successfully updated. Enjoy working with the latest Data')
+        else:
+            st.stop()
+
+    time.sleep(2)
+
+    time.sleep(5) 
+    success_message.empty()
+    time.sleep(2)  
+
+st.write("Click to access Latest Stock data")
+if st.button("Stock data"):
+    st.dataframe(data)
+
+st.markdown('<h1 style="text-align: center; color: #800080;">Candle Stick Plot</h1>', unsafe_allow_html=True)
 fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
                     subplot_titles=['Candlestick Chart', 'Moving Average'])
 fig.add_trace(go.Candlestick(x=data.index, 
-                             open=data['Open'], 
-                             high=data['High'], 
-                             low=data['Low'], 
-                             close=data['Close'], 
-                             name='Candlesticks'), row=1, col=1)
+                            open=data['Open'], 
+                            high=data['High'], 
+                            low=data['Low'], 
+                            close=data['Close'], 
+                            name='Candlesticks'), row=1, col=1)
 
-# Moving Average
 period = st.sidebar.slider('Select Moving Average Period', min_value=5, max_value=50, step=5, value=20)
 data['SMA'] = SMAIndicator(data['Close'], window=period).sma_indicator()
 fig.add_trace(go.Scatter(x=data.index, y=data['SMA'], mode='lines', name=f'{period} days SMA', line=dict(color='orange')), row=2, col=1)
 
-# Set layout for Candlestick Chart
 fig.update_xaxes(type='category')
-fig.update_layout(xaxis_rangeslider_visible=False, height=600, title_text=f'{ticker} Stock Analysis')
+fig.update_layout(xaxis_rangeslider_visible=False, height=1500, width=800, title_text=f'{ticker} Stock Analysis') 
 st.plotly_chart(fig)
 
-# Display stock data
-st.subheader('Stock Data')
-st.write(data)
 
-# Additional Analysis
-st.header('Additional Analysis')
 
-# Calculate and display moving average
+def exponential_smoothing(series, alpha):
+    result = [series[0]] # first value is same as series
+    for n in range(1, len(series)):
+        result.append(alpha * series[n] + (1 - alpha) * result[n-1])
+    return result
+
+
+
+# Sidebar for smoothing parameter (alpha)
+alpha = st.sidebar.slider('Select Smoothing Parameter (alpha)', min_value=0.1, max_value=1.0, step=0.1, value=0.5)
+
+
+# Apply exponential smoothing to the 'Close' prices
+data['Smoothed_Close'] = exponential_smoothing(data['Close'], alpha)
+
+
+st.markdown('<h1 style="text-align: center; color: #800080;">Exponential Smoothing Curve</h1>', unsafe_allow_html=True)
+
+# Plot the original and smoothed data
+st.line_chart(data[['Close', 'Smoothed_Close']])
+
+# Display the smoothed data
+if st.button('Click Me to See Smoothed Closing Prices'):
+    st.write("Smoothed Close Prices:")
+    st.write(data[['Close', 'Smoothed_Close']])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Additional Analysis
+st.markdown('<h1 style="text-align: center; color: #800080;">Additional Analysis</h1>', unsafe_allow_html=True)
+
+#Moving Average
 ma_period = st.slider('Select Moving Average Period for Analysis', min_value=5, max_value=50, step=5, value=20)
 data['MA'] = data['Close'].rolling(window=ma_period).mean()
 st.write(f'{ma_period} days Moving Average:')
@@ -87,33 +203,36 @@ rsi = 100 - (100 / (1 + rs))
 st.write('Relative Strength Index (RSI):')
 st.line_chart(rsi)
 
+
+
+st.markdown('<h1 style="text-align: center; color: #800080;">Buying and Selling Point Signals</h1>', unsafe_allow_html=True)
+st.write("Ensure that you adjust the overbought and oversold levels appropriately")
 # Add trading signals based on RSI
 overbought_level = st.slider('Select Overbought Level', min_value=50, max_value=70, step=1, value=70)
 oversold_level = st.slider('Select Oversold Level', min_value=30, max_value=50, step=1, value=30)
 data['RSI'] = rsi
 data['Buy_Signal'] = data['RSI'] < oversold_level
 data['Sell_Signal'] = data['RSI'] > overbought_level
-
 # Plot Buy and Sell signals
 fig_signals = go.Figure(data=[go.Candlestick(x=data.index, 
-                                             open=data['Open'], 
-                                             high=data['High'], 
-                                             low=data['Low'], 
-                                             close=data['Close'], 
-                                             name='Candlesticks')])
+                                            open=data['Open'], 
+                                            high=data['High'], 
+                                            low=data['Low'], 
+                                            close=data['Close'], 
+                                            name='Candlesticks')])
 fig_signals.add_trace(go.Scatter(x=data.index, y=data['MA'], mode='lines', name=f'{ma_period} days SMA', line=dict(color='orange')))
 fig_signals.add_trace(go.Scatter(x=data[data['Buy_Signal']].index, 
-                                 y=data[data['Buy_Signal']]['Low'], 
-                                 mode='markers', 
-                                 marker=dict(color='green', size=10), 
-                                 name='Buy Signal'))
+                                y=data[data['Buy_Signal']]['Low'], 
+                                mode='markers', 
+                                marker=dict(color='green', size=10), 
+                                name='Buy Signal'))
 fig_signals.add_trace(go.Scatter(x=data[data['Sell_Signal']].index, 
-                                 y=data[data['Sell_Signal']]['High'], 
-                                 mode='markers', 
-                                 marker=dict(color='red', size=10), 
-                                 name='Sell Signal'))
+                                y=data[data['Sell_Signal']]['High'], 
+                                mode='markers', 
+                                marker=dict(color='red', size=10), 
+                                name='Sell Signal'))
 fig_signals.update_xaxes(type='category')
-fig_signals.update_layout(xaxis_rangeslider_visible=False, height=600, title_text=f'{ticker} Stock Analysis with Signals')
+fig_signals.update_layout(xaxis_rangeslider_visible=False, height=700, width = 1300, title_text=f'{ticker} Stock Analysis with Signals')
 st.plotly_chart(fig_signals)
 
 
@@ -125,9 +244,6 @@ except Exception as e:
     st.sidebar.error(f'Error loading data: {e}')
     st.stop()
 
-import streamlit as st
-import pandas as pd 
-import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import yfinance as yf
@@ -191,3 +307,22 @@ plt.tight_layout()
 plt.legend()
 st.pyplot(fig)
 
+
+
+
+def main():
+    feedback = st.text_area('Provide Your Feedback', 'Write your feedback here...')
+    
+    # Button to submit feedback
+    if st.button('Submit Feedback'):
+        # You can handle the feedback submission here (e.g., store it in a database)
+        st.success('Feedback Submitted! Thank you for your input.')
+    
+    # Button for people to contact
+    if st.button('Contact Us'):
+        # You can provide contact information or a contact form here
+        st.write('For inquiries, please email us at sammykyalo47@gmail.com')
+        
+
+if __name__ == '__main__':
+    main()
