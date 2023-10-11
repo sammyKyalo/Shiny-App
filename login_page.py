e7d2b79a9f019751724eb2f11b4339a3f8f99ebf
Sammy Kyalo <sammykyalo47@gmail.com>
2023-10-11 11:51:57 +0300

    up

--- login_page.py
+++ login_page.py
@@ -74,20 +74,23 @@ if selection == "Login":
     st.header("Login")
     username = st.text_input('Username')
     password = st.text_input('Password', type='password')
-    if st.button('Login'):
+    login_button = st.button('Login')
+
+    if login_button:
         if verify_user(username, password):
             st.success(f'Welcome, {username}!')
             
             # Run shiny.py and display the output here
             session_time = time.time()
 
-        # Display the link only if it has not expired
+            # Display the link only if it has not expired
             if not is_link_expired(session_time):
-              st.markdown(f'[Click here to go to the analysis page](https://telsa-stock-analysis.streamlit.app/)')
+                st.markdown(f'[Click here to go to the analysis page](https://telsa-stock-analysis.streamlit.app/)')
             else:
-               st.warning('Link has expired. Please login again to generate a new link.')
-    else:
-        st.warning('Invalid username or password. Please try again or sign up.')
+                st.warning('Link has expired. Please login again to generate a new link.')
+        else:
+            st.warning('Invalid username or password. Please try again or sign up.')
+
 # Signup Section
 else:
     st.header("Sign Up")
