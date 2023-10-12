0cfeafec28ac5419b7afc0614144f8931f946c49
Sammy Kyalo <sammykyalo47@gmail.com>
2023-10-12 09:39:53 +0300

    up

--- login_page.py
+++ login_page.py
@@ -85,7 +85,7 @@ if selection == "Login":
 
             # Display the link only if it has not expired
             if not is_link_expired(session_time):
-                st.markdown('https://telsa-stock-analysis.streamlit.app/', unsafe_allow_html=True, target="_blank")
+                st.write('[Analysis Page](https://telsa-stock-analysis.streamlit.app/)')
             else:
                 st.warning('Link has expired. Please login again to generate a new link.')
         else:
